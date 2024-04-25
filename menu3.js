fd9de8dcccbc280c315867a7ff2f1ff933df3539
Dmorzy <168143230+Dmorzy@users.noreply.github.com>
2024-04-25 17:00:04 -0600

    Add files via upload

---
+++ menu3.js
@@ -0,0 +1,15 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Monaco;}
+{\colortbl;\red255\green255\blue255;\red39\green129\blue201;\red12\green12\blue12;\red255\green255\blue255;
+\red235\green16\blue47;\red212\green20\blue102;\red20\green152\blue106;}
+{\*\expandedcolortbl;;\cssrgb\c18039\c58431\c82745;\cssrgb\c5098\c5098\c5098;\cssrgb\c100000\c100000\c100000;
+\cssrgb\c94902\c17255\c23922;\cssrgb\c87451\c18824\c47451;\cssrgb\c0\c65098\c49020;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 function\cf4 \strokec4  \cf5 \strokec5 toggleMenu\cf4 \strokec4 () \{\
+    \cf2 \strokec2 var\cf4 \strokec4  menuList = \cf6 \strokec6 document\cf4 \strokec4 .\cf5 \strokec5 getElementById\cf4 \strokec4 (\cf7 \strokec7 "menuList"\cf4 \strokec4 );\
+    menuList.classList.\cf5 \strokec5 toggle\cf4 \strokec4 (\cf7 \strokec7 "hidden"\cf4 \strokec4 );\
+\}}
